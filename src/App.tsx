97892e94cadceb427a53c4a39132dd242570856b
Alex Baltas <56007143+AlexBaltas@users.noreply.github.com>
2025-05-18 20:29:05 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
-
-useEffect(() => {
+  useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
